3a2cab6e723a99d548cd4b49a89af97802b129ce
MereteTeudt <mere0312@edu.campusvejle.dk>
2018-08-23 11:06:34 +0200

    Add project files.

---
+++ EmployeeExercise/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExercise
+{
+    public abstract class Employee
+    {
+        public string Firstname { get; }
+        public string Lastname { get; }
+        public string SocialSecurityNumber { get; }
+
+        public Employee(string firstName, string lastName, string socialSecurityNumber)
+        {
+            Firstname = firstName;
+            Lastname = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Firstname} {Lastname} \n" + $"social security number: {SocialSecurityNumber}";
+        }
+
+        public abstract decimal Earnings();
+    }
+}
---
+++ EmployeeExercise/SalariedEmployee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExercise
+{
+    class SalariedEmployee : Employee
+    {
+        private decimal weeklySalary;
+
+        public SalariedEmployee(string firstName, string lastName, string socialSecurityNumber, decimal weeklySalary)
+            : base(firstName, lastName, socialSecurityNumber)
+        {
+            WeeklySalary = weeklySalary;
+        }
+
+        public decimal WeeklySalary
+        {
+            get
+            {
+                return weeklySalary;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(WeeklySalary)} must be >= 0");
+                }
+                weeklySalary = value;
+            }
+        }
+
+        public override decimal Earnings()
+        {
+            return WeeklySalary;   
+        }
+
+        public override string ToString()
+        {
+            return $"salaried employee: {base.ToString()}\n" +
+                $"weekly salary: {WeeklySalary:C}";
+        }
+    }
+}
